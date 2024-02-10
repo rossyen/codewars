b5d6c95cf76ae1a2c92a39179c364a362dd96eb8
rossyen <rosselandjt@gmail.com>
2024-02-10 19:19:45 +0100

    First attempt at code to convert bool to string
    adds first attempt at converting bool values to
    string

---
+++ bool_string_converter.py
@@ -0,0 +1,12 @@
+# bool_string_converter.py
+'''
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+'''
+
+# first attempt
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
